2f5d8b710b935fad413a94c7c28828de8d1f43bc
KatraSterner <Rollie406@gmail.com>
2026-04-20 16:21:39 -0500

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,4 @@
-"use client";
 
-import { useState, useEffect } from "react";
-// import components here
-
-export default function Home() {
-
-  // define stateful variables here
-  // const [gallery, setGallery] = useState([]);
-
-  useEffect(() => {
-    const url = 'https://api.artsearch.io/artworks/random';
-    const key = process.env.NEXT_PUBLIC_API_KEY;
-    if (!key) {
-      throw new Error("Missing NEXT_PUBLIC_API_KEY");
-    }
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        "api-key": process.env.NEXT_PUBLIC_API_KEY ?? "",
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error('There was a problem with the fetch operation:', error));
-
-  }, [])
-
-  return (
-    <div>
-      <h1>Katra Sterner</h1>
-    </div>
-  );
+export default function Page() {
+  return <h1>DEPLOY TEST SUCCESS</h1>;
 }
